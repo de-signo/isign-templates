4c616b61df2d47b4bdb606732109da65fefb00c4
Michael Stoll <michael.stoll@stolltec.com>
2022-07-26 12:54:46 +0200

    stadt: Abstimmung der Scrollfunktion

--- Stadt/src/app/list-view/list-view.component.ts
+++ Stadt/src/app/list-view/list-view.component.ts
@@ -1,6 +1,7 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { timer } from 'rxjs';
 import { Category, Item } from '../data/app-data.model';
 import { DataService } from '../data/data.service';
 
@@ -9,12 +10,19 @@ import { DataService } from '../data/data.service';
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss']
 })
-export class ListViewComponent {
+export class ListViewComponent implements AfterViewInit {
   enableAnchors = false;
   category: Category = new Category();
   items: ItemWithHook[] = [];
   hooks: {[a: string]: boolean} = {};
   handicapped = false;
+
+  // scroll
+  readonly handicappedScrollOffset: number = 500;
+  scrollTop: number = this.handicappedScrollOffset;
+  showScrollup = true;
+  showScrolldown = true;
+  @ViewChild("divScroll") divScroll!: ElementRef<HTMLElement>;
 
   constructor(svc: DataService, route: ActivatedRoute, public scroller: ViewportScroller) {
     route.queryParams.subscribe(
@@ -28,6 +36,33 @@ export class ListViewComponent {
         );
       }
     );
+  }
+
+  ngAfterViewInit() {
+    // timer to start new change detection context
+    timer(1).subscribe(_ => {
+      this.scroll(0);
+    });
+  }
+
+  scroll(dir: -1|0|1) {
+    const el = this.divScroll.nativeElement;
+    const max = el.scrollHeight - el.clientHeight;
+    var st = this.scrollTop + dir * 500;
+    this.scrollTop = Math.max(0, Math.min(max, st));
+    this.showScrollup = this.scrollTop != 0;
+    this.showScrolldown = this.scrollTop != max;
+
+    // cannot use binding / change detection for this, because it is updated by view
+    el.scrollTop = this.scrollTop;
+  }
+
+  showHandicapped(): void {
+    this.handicapped = true;
+    if (this.scrollTop == this.handicappedScrollOffset) {
+      this.scrollTop = 0;
+      this.scroll(0);
+    }
   }
 
   buildItems(items: Item[]) {
